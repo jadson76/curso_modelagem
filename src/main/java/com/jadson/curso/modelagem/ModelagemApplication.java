a9f867a6dc4206e84b76f414a9099152dc2d6dac
jadson76 <biojadson@gmail.com>
2022-08-04 19:51:34 -0300

    Projeto criado

---
+++ src/main/java/com/jadson/curso/modelagem/ModelagemApplication.java
@@ -0,0 +1,13 @@
+package com.jadson.curso.modelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jadson/curso/modelagem/ModelagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadson.curso.modelagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
