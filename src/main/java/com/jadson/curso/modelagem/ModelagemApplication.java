9e4374ecfa907dcddb8366480eba396757c11955
jadson76 <biojadson@gmail.com>
2022-08-07 18:13:46 -0300

    Produto e associações
    Classe Produto. Refina inicialização da base.

--- src/main/java/com/jadson/curso/modelagem/ModelagemApplication.java
+++ src/main/java/com/jadson/curso/modelagem/ModelagemApplication.java
@@ -1,13 +1,47 @@
 package com.jadson.curso.modelagem;
 
+import com.jadson.curso.modelagem.domain.Categoria;
+import com.jadson.curso.modelagem.domain.Produto;
+import com.jadson.curso.modelagem.repository.CategoriaRepository;
+import com.jadson.curso.modelagem.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ModelagemApplication {
+public class ModelagemApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		Produto p1 = new Produto(null,"Computador",1000.00);
+		Produto p2 = new Produto(null,"Impressora",400.00);
+		Produto p3 = new Produto(null, "Mouse", 50.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+	}
 }
