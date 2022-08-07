91f629d7f267c4b68aa05a719a8fa0df84e58ad0
jadson76 <biojadson@gmail.com>
2022-08-07 10:56:24 -0300

    Categoria
    Incluindo primeiro dominio.

--- src/main/java/com/jadson/curso/modelagem/resources/CategoriaResource.java
+++ src/main/java/com/jadson/curso/modelagem/resources/CategoriaResource.java
@@ -1,16 +1,23 @@
 package com.jadson.curso.modelagem.resources;
 
+import com.jadson.curso.modelagem.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+        List<Categoria> lista = Arrays.asList(cat1,cat2);
+        return lista;
     }
 
 }
